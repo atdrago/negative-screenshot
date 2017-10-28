73d78c44f04aaf8b36251060b24eb9442f8ffba6
Adam Drago <atdrago@gmail.com>
2017-10-27 22:41:54 -0700

    Use window title to find correct window

--- swift/negative-screenshot/main.swift
+++ swift/negative-screenshot/main.swift
@@ -5,10 +5,20 @@ let x = Int(args[1]) ?? 0
 let y = Int(args[2]) ?? 0
 let width = Int(args[3]) ?? 100
 let height = Int(args[4]) ?? 100
-let windowId = Int(args[5])
-    
+let windowTargetTitle = "Negative - Capture"
+
+// Get the windowId of the window we want to ignore. Default it to 0 which would not ignore any window
+var windowId = 0
+
+if let windowInfoList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) as? [[ String : Any ]] {
+    // Get the window info for window with our target name
+    if let windowInfo = windowInfoList.first(where: { $0["kCGWindowName"] as! String == windowTargetTitle }) {
+        windowId = (windowInfo["kCGWindowNumber"] as! Int)
+    }
+}
+
 let rect = NSMakeRect(CGFloat(x), CGFloat(y), CGFloat(width), CGFloat(height))
-let cgImage = CGWindowListCreateImage(rect, .optionOnScreenBelowWindow, CGWindowID(windowId ?? 0), .bestResolution)
+let cgImage = CGWindowListCreateImage(rect, .optionOnScreenBelowWindow, CGWindowID(windowId), .bestResolution)
 
 if let image = cgImage {
     let imageRep = NSBitmapImageRep(cgImage: image)
