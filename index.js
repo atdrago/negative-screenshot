aa96b5a1d73928a2a82501de1e1a62b1aaed5cf3
Adam Drago <atdrago@gmail.com>
2017-10-27 22:36:41 -0700

    Remove nodobjc

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const objc = require('nodobjc');
 const path = require('path');
 const { spawn } = require('child_process');
 
-objc.framework('Cocoa');
-
 module.exports = function (x, y, width, height) {
     return new Promise(function (resolve, reject) {
-        // TODO: Figure out how we can get windowNumber from the application itself
-        const pool = objc.NSAutoreleasePool('alloc')('init');
-        const windowId = objc.NSApplication('sharedApplication')('keyWindow')('windowNumber');
-        pool('drain');
-
         const cmd = spawn(
             path.join(__dirname, 'negative-screenshot'),
-            [ x, y, width, height, windowId ]
+            [ x, y, width, height ]
         );
 
         cmd.stderr.on('data', function (err) {
