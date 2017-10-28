f25a26f13094c8ca6099f81e80702f730d5db881
Adam Drago <atdrago@gmail.com>
2017-10-28 11:35:21 -0700

    Refactor - Use execa - Serialize arguments pass to swift script - Generate unique id for window title

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
+const execa = require('execa');
 const path = require('path');
-const { spawn } = require('child_process');
+const uuid = require('uuid/v4');
 
-module.exports = function (x, y, width, height) {
+const { Rect } = require('./models');
+
+function createSerializableOptions(options, uniqueWindowTitle) {
+    const serializableOptions = {};
+
+    if (options.belowWindowWithId) {
+        serializableOptions.belowWindowWithId = options.belowWindowWithId
+    } else if (uniqueWindowTitle) {
+        serializableOptions.uniqueWindowTitle = uniqueWindowTitle;
+    }
+
+    serializableOptions.bounds = new Rect(options.bounds).toSerializable();
+
+    return serializableOptions;
+}
+
+/**
+ * Take a screenshot
+ * @param  {Object}         options
+ * @param  {Rect}           options.bounds
+ * @param  {BrowserWindow}  options.belowWindow
+ * @param  {Number}         options.belowWindowWithId
+ * @return {Promise}        [description]
+ */
+module.exports = function (options = {}) {
     return new Promise(function (resolve, reject) {
-        const cmd = spawn(
-            path.join(__dirname, 'negative-screenshot'),
-            [ x, y, width, height ]
-        );
+        const belowWindow = options.belowWindow;
+        const originalWindowTitle = belowWindow && belowWindow.getTitle();
+        const isUniqueWindowTitleNeeded = !!originalWindowTitle;
+        const uniqueWindowTitle = isUniqueWindowTitleNeeded && uuid();
 
-        cmd.stderr.on('data', function (err) {
+        if (isUniqueWindowTitleNeeded) {
+            belowWindow.setTitle(uniqueWindowTitle);
+        }
+
+        const cmd = execa(
+            path.join(__dirname, 'negative-screenshot'),
+            [ JSON.stringify(createSerializableOptions(options, uniqueWindowTitle)) ]
+        );
+        const dataArray = [];
+
+        cmd.stdout.setEncoding('utf8');
+
+        // Handle errors
+        cmd.stderr.on('data', err => {
+            if (isUniqueWindowTitleNeeded) {
+                belowWindow.setTitle(originalWindowTitle);
+            }
+
             reject(err);
         });
 
-        const dataArray = [];
-        cmd.stdout.on('data', (data) => {
-            dataArray.push(data.toString());
-        });
-        cmd.on('close', () => {
-            const data = dataArray.join('').trim();
-            resolve(data);
+        // Load all the image data into an array
+        cmd.stdout.on('data', data => dataArray.push(data.toString()));
+
+        // Resolve with all image data
+        cmd.stdout.on('close', () => {
+            if (isUniqueWindowTitleNeeded) {
+                belowWindow.setTitle(originalWindowTitle);
+            }
+
+            resolve(dataArray.join('').trim());
         });
     });
 }
